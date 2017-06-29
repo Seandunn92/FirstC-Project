8436b0741497ec3dafe6f4c3dd2f6892c20d0ca3
Seandunn92 <seandunn92@gmail.com>
2017-06-29 16:16:25 -0400

    First commit

---
+++ ConsoleApplication2/Cat.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Cat
+    {
+
+        private String sound;
+        private int tailLength;
+        public Cat()
+        {
+            sound = "meow";
+            tailLength = 2;
+        }
+        public String getSound()
+        {
+            return sound;
+        }
+        public int getTailLength()
+        {
+            return tailLength;
+        }
+
+        
+
+    }
+}
---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+
+
+
+        static bool isPalindrome(String s)
+        {
+            for(int i=0; i<s.Length; i++)
+            {
+                if (!(s.ElementAt(i) == s.ElementAt(s.Length - i - 1)))
+                    return false;
+            }
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HelloWorld");
+            Cat ourCat = new Cat();
+            Console.WriteLine("The cat says " + ourCat.getSound());
+            Console.WriteLine("The cats tail is " + ourCat.getTailLength() + " inches long");
+            String s; 
+            bool b;
+            while (true)
+            {
+                Console.WriteLine("Enter a string");
+               s= Console.ReadLine();
+                b = isPalindrome(s);
+                Console.Write(s);
+                if (b)
+                    Console.WriteLine(" is a Palindrome");
+                else
+                    Console.WriteLine(" is not a Palindrome");
+            }
+
+        }
+    }
+}
